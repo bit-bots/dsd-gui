745b030971d81ebdb73c02f97859962a9d2de395
texhnolyze <joegriepen@gmail.com>
2022-11-24 23:02:44 +0100

    fix(electron): initilization of the app
    with upgrades to electron 21

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,10 @@
-"use strict";
-
-import { app, protocol, BrowserWindow } from "electron";
-import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import Store from "electron-store";
+import { app, BrowserWindow, protocol, App } from "electron";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
-const Store = require("electron-store");
+const isNodeIntegrated = Boolean(process.env.ELECTRON_NODE_INTEGRATION);
 
 Store.initRenderer();
 // Scheme must be registered before the app is ready
@@ -12,73 +12,79 @@ protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
 
+registerAppEventHandlers(app);
+
+async function renderApp(browserWindow: BrowserWindow) {
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    await browserWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    browserWindow.webContents.openDevTools();
+  } else {
+    createProtocol("app");
+    // Load the index.html when not in development
+    browserWindow.loadURL("app://./index.html");
+  }
+}
+
 async function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const browserWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
     frame: false,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      enableRemoteModule: true,
-      webSecurity: false,
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION as unknown as boolean,
+      nodeIntegration: isNodeIntegrated,
+      contextIsolation: !isNodeIntegrated,
     },
   });
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
-    win.webContents.openDevTools();
-  } else {
-    createProtocol("app");
-    // Load the index.html when not in development
-    win.loadURL("app://./index.html");
-  }
+  await renderApp(browserWindow);
 }
 
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS);
-    } catch (e) {
-      console.error("Vue Devtools failed to install:", e.toString());
-    }
-  }
-  createWindow();
-});
-
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === "win32") {
-    process.on("message", (data) => {
-      if (data === "graceful-exit") {
-        app.quit();
-      }
-    });
-  } else {
-    process.on("SIGTERM", () => {
+function registerAppEventHandlers(app: App) {
+  // Quit when all windows are closed.
+  app.on("window-all-closed", () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
       app.quit();
-    });
+    }
+  });
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on("ready", async () => {
+    if (isDevelopment && !process.env.IS_TEST) {
+      try {
+        await installExtension(VUEJS_DEVTOOLS);
+      } catch (e) {
+        console.error("Vue Devtools failed to install:", e);
+      }
+    }
+
+    createWindow();
+  });
+
+  // Exit cleanly on request from parent process in development mode.
+  if (isDevelopment) {
+    if (process.platform === "win32") {
+      process.on("message", (data) => {
+        if (data === "graceful-exit") {
+          app.quit();
+        }
+      });
+    } else {
+      process.on("SIGTERM", () => {
+        app.quit();
+      });
+    }
   }
 }
